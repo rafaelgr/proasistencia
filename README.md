31bf45c496483a88638014e7443398b4b2f6419b
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-04-25 12:30:55 +0200

    VRS 0.0.1 Solo login y mantenimiento de usuarios

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# proasistencia" 
+Servidor estático y  API de la aplicación PROASISTENCIA  
+
+ 
+<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">PROASISTENCIA API</span> por <a xmlns:cc="http://creativecommons.org/ns#" href="www.ariadnasw.com" property="cc:attributionName" rel="cc:attributionURL">Ariadna Software S.L.</a> se distribuye bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Licencia Creative Commons Atribución-NoComercial 4.0 Internacional</a>.<br />Basada en una obra en <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/rafaelgr/proasistencia" rel="dct:source">https://github.com/rafaelgr/proasistencia</a>.
---
+++ index.js
@@ -0,0 +1,67 @@
+//=======================================
+// Proasistencia (index.js)
+// API to communicate to PROASISTENCIA
+//========================================
+// Author: Rafael Garcia (rafa@myariadna.com)
+// 2015 [License CC-BY-NC-4.0]
+
+
+// required modules
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var serveIndex = require('serve-index');
+
+// api support
+var usuarios_router = require('./lib/usuarios/usuarios_controller');
+var version_router = require('./lib/version/version_controller');
+var vigilantes_router = require('./lib/vigilantes/vigilantes_controller');
+
+// read app parameters (host and port for the API)
+var config = require('./config.json');
+
+
+// starting express
+var app = express();
+// to parse body content
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+// using cors for cross class
+app.use(cors());
+
+// servidor html estático
+app.use(express.static(__dirname+"/public"));
+app.use('/ficheros', serveIndex(__dirname + '/public/ficheros',{'icons': true, 'view': 'details'}));
+
+
+
+// mounting routes
+var router = express.Router();
+
+// -- common to all routes
+router.use(function(req, res, next) {
+    // go on (by now)
+    next();
+});
+
+
+// -- general GET (to know if the server is up and runnig)
+router.get('/', function(req, res) {
+    res.json('PROASISTENCIA API / SERVER -- runnig');
+});
+
+// -- registering routes
+app.use('/api', router);
+app.use('/api/usuarios', usuarios_router);
+app.use('/api/version', version_router);
+
+// -- start server
+app.listen(config.apiPort);
+
+// -- console message
+console.log('PROASISTENCIA API / SERVER on port: ' + config.apiPort);
