8a4e005685c41fbcd84cf8c91f767d6329f723c1
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-09-28 11:27:17 +0200

    Importe mantenedor y fecha original

--- sql/1.1.1.sql
+++ sql/1.1.1.sql
@@ -2,3 +2,9 @@ ALTER TABLE `proasistencia`.`contrato_comercial`
 ADD COLUMN `comision` DECIMAL(5,2) NULL AFTER `manComercial`;
 ALTER TABLE `proasistencia`.`contrato_comercial` 
 ADD UNIQUE INDEX `idx_empresa_comercial` (`empresaId`, `comercialId`); 
+ALTER TABLE `proasistencia`.`contrato_cliente_mantenimiento`   
+  ADD COLUMN `importeMantenedor` DECIMAL(10,2) NULL AFTER `impComer`;
+ALTER TABLE `proasistencia`.`contrato_cliente_mantenimiento`   
+  ADD COLUMN `fechaOriginal` DATE NULL AFTER `importeMantenedor`;
+ALTER TABLE `proasistencia`.`clientes`   
+  ADD COLUMN `codigo` INT(11) NULL AFTER `cuentaCorriente`;  
