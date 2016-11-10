1bd0f5d71225e2987a95426ec3e5c0a4f152026e
Rafa <rafa@myariadna.com>
2016-11-10 16:52:23 +0100

    Mantenimiento de motivos de baja

---
+++ public/js/ariadna/motivoBajaDetalle.js
@@ -0,0 +1,137 @@
+﻿/*-------------------------------------------------------------------------- 
+motivoBajaDetalle.js
+Funciones js par la página MotivoBajaDetalle.html
+---------------------------------------------------------------------------*/
+var empId = 0;
+
+datePickerSpanish(); // see comun.js
+
+function initForm() {
+    comprobarLogin();
+    // de smart admin
+    pageSetUp();
+    // 
+    getVersionFooter();
+    vm = new admData();
+    ko.applyBindings(vm);
+    // asignación de eventos al clic
+    $("#btnAceptar").click(aceptar());
+    $("#btnSalir").click(salir());
+    $("#frmMotivoBaja").submit(function() {
+        return false;
+    });
+
+    empId = gup('MotivoBajaId');
+    if (empId != 0) {
+        var data = {
+                motivoBajaId: empId
+            }
+            // hay que buscar ese elemento en concreto
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/motivos_baja/" + empId,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function(data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadData(data);
+            },
+            error: errorAjax
+        });
+    } else {
+        // se trata de un alta ponemos el id a cero para indicarlo.
+        vm.motivoBajaId(0);
+    }
+}
+
+function admData() {
+    var self = this;
+    self.motivoBajaId = ko.observable();
+    self.nombre = ko.observable();
+}
+
+function loadData(data) {
+    vm.motivoBajaId(data.motivoBajaId);
+    vm.nombre(data.nombre);
+}
+
+function datosOK() {
+    $('#frmMotivoBaja').validate({
+        rules: {
+            txtNombre: {
+                required: true
+            }
+        },
+        // Messages for form validation
+        messages: {
+            txtNombre: {
+                required: "Debe dar un nombre"
+            }
+        },
+        // Do not change code below
+        errorPlacement: function(error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+    var opciones = $("#frmMotivoBaja").validate().settings;
+    return $('#frmMotivoBaja').valid();
+}
+
+
+function aceptar() {
+    var mf = function() {
+        if (!datosOK())
+            return;
+        var data = {
+            motivoBaja: {
+                "motivoBajaId": vm.motivoBajaId(),
+                "nombre": vm.nombre()
+            }
+        };
+        if (empId == 0) {
+            $.ajax({
+                type: "POST",
+                url: myconfig.apiUrl + "/api/motivos_baja",
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function(data, status) {
+                    // hay que mostrarlo en la zona de datos
+                    loadData(data);
+                    // Nos volvemos al general
+                    var url = "MotivoBajaGeneral.html?MotivoBajaId=" + vm.motivoBajaId();
+                    window.open(url, '_self');
+                },
+                error: errorAjax
+            });
+        } else {
+            $.ajax({
+                type: "PUT",
+                url: myconfig.apiUrl + "/api/motivos_baja/" + empId,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function(data, status) {
+                    // hay que mostrarlo en la zona de datos
+                    loadData(data);
+                    // Nos volvemos al general
+                    var url = "MotivoBajaGeneral.html?MotivoBajaId=" + vm.motivoBajaId();
+                    window.open(url, '_self');
+                },
+                error: errorAjax
+            });
+        }
+    };
+    return mf;
+}
+
+
+function salir() {
+    var mf = function() {
+        var url = "MotivoBajaGeneral.html";
+        window.open(url, '_self');
+    }
+    return mf;
+}
+
