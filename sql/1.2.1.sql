d4d9eec966d98de7185be1806a55a46618a0e66b
Rafa <rafa@myariadna.com>
2017-01-18 13:10:21 +0100

    PROAS-208 Creardas las columnas para el control de unidades en las líneas de oferta y contrato.

---
+++ sql/1.2.1.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `ofertas_lineas`   
+  ADD COLUMN `unidadId` INT NULL AFTER `ofertaId`,
+  ADD CONSTRAINT `ofl_unidad` FOREIGN KEY (`unidadId`) REFERENCES `unidades`(`unidadId`);
+
+ALTER TABLE `contratos_lineas`   
+  ADD COLUMN `unidadId` INT(11) NULL AFTER `contratoId`,
+  ADD CONSTRAINT `cntl_unidades` FOREIGN KEY (`unidadId`) REFERENCES `unidades`(`unidadId`);  
