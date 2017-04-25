fdf769a56d9e0d116fd6f5ceb3b74ecaab17b739
Rafael Garcia <rafa@myariadna.com>
2017-04-25 16:26:07 +0200

    Informe de oferta

--- public/js/ariadna/rptOfertas.js
+++ public/js/ariadna/rptOfertas.js
@@ -5,19 +5,188 @@ var responsiveHelper_datatable_fixed_column = undefined;
 var responsiveHelper_datatable_col_reorder = undefined;
 var responsiveHelper_datatable_tabletools = undefined;
 
-var dataRondasRealizadas;
-var rondaRealizadaId;
-
 var breakpointDefinition = {
     tablet: 1024,
     phone: 480
 };
-
 
 function initForm() {
     comprobarLogin();
     // de smart admin
     pageSetUp();
     getVersionFooter();
+    $.validator.addMethod("greaterThan",
+        function (value, element, params) {
+            var fv = moment(value, "DD/MM/YYYY").format("YYYY-MM-DD");
+            var fp = moment($(params).val(), "DD/MM/YYYY").format("YYYY-MM-DD");
+            if (!/Invalid|NaN/.test(new Date(fv))) {
+                return new Date(fv) >= new Date(fp);
+            } else {
+                // esto es debido a que permitimos que la segunda fecha nula
+                return true;
+            }
+        }, 'La fecha final debe ser mayor que la inicial.');
+    datePickerSpanish();
+    vm = new admData();
+    ko.applyBindings(vm);
+    //
+    $("#btnImprimir").click(obtainReport);
+    // avoid form submmit
+    $("#frmRptOfertas").submit(function () {
+        return false;
+    });
+    //
+    $('#txtRFecha').daterangepicker({
+        "showDropdowns": true,
+        "locale": {
+            "direction": "ltr",
+            "format": "DD/MM/YYYY",
+            "separator": " - ",
+            "applyLabel": "Aceptar",
+            "cancelLabel": "Cancelar",
+            "fromLabel": "Desde",
+            "toLabel": "Hasta",
+            "customRangeLabel": "Personalizado",
+            "daysOfWeek": [
+                "Do",
+                "Lu",
+                "Ma",
+                "Mi",
+                "Ju",
+                "Vi",
+                "Sa"
+            ],
+            "monthNames": [
+                "Enero",
+                "Febrero",
+                "Marzo",
+                "Abril",
+                "Mayo",
+                "Junio",
+                "Julio",
+                "Agosto",
+                "Septiembre",
+                "Octubre",
+                "Noviembre",
+                "Diciembre"
+            ],
+            "firstDay": 1
+        },
+        "alwaysShowCalendars": true,
+        ranges: {
+            'Hoy': [moment(), moment()],
+            'Ayer': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Esta semana': [moment().startOf('week'), moment().endOf('week')],
+            'Semana pasada': [moment().subtract(1, 'week').startOf('week'), moment().subtract(1, 'week').endOf('week')],
+            'Este mes': [moment().startOf('month'), moment().endOf('month')],
+            'Último mes': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+            'Este año': [moment().startOf('year'), moment().endOf('year')],
+            'Último año': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
+        }
+    }, function (start, end, label) {
+        //alert('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+        vm.dFecha(start.format('YYYY-MM-DD'));
+        vm.hFecha(end.format('YYYY-MM-DD'));
+    });
+    vm.dFecha(moment().format('YYYY-MM-DD'));
+    vm.hFecha(moment().format('YYYY-MM-DD'));
+
+    //
+    $("#cmbEmpresas").select2(select2Spanish());
+    loadEmpresas();
+    initAutoCliente();
 }
 
+function admData() {
+    var self = this;
+    self.dFecha = ko.observable();
+    self.hFecha = ko.observable();
+    //
+    self.empresaId = ko.observable();
+    self.sempresaId = ko.observable();
+    //
+    self.posiblesEmpresas = ko.observableArray([]);
+    self.elegidosEmpresas = ko.observableArray([]);
+    //
+    self.clienteId = ko.observable();
+    self.sclienteId = ko.observable();
+    //
+    self.posiblesClientes = ko.observableArray([]);
+    self.elegidosClientes = ko.observableArray([]);
+};
+
+var obtainReport = function () {
+    if (!datosOK()) return;
+    //alert('Desde: ' + vm.dFecha() + ' Hasta:' + vm.hFecha());
+    //alert('Empresa: ' + vm.sempresaId() + ' Cliente:' + vm.sclienteId() );
+    var url = "VisReport.html?report=oferta_general";
+    url += "&dFecha=" + vm.dFecha();
+    url += "&hFecha=" + vm.hFecha();
+    if (vm.sempresaId()) url += "&empresaId=" + vm.sempresaId();
+    if ($('#txtCliente').val() != "" && vm.sclienteId()) url += "&clienteId=" + vm.sclienteId();
+    //printReport(url);
+    window.open(url, '_new');
+};
+
+var printReport = function (url) {
+    $("#reportArea").attr('src', url);
+};
+
+function datosOK() {
+    $('#frmRptOfertas').validate({
+        rules: {
+        },
+        // Messages for form validation
+        messages: {
+        },
+        // Do not change code below
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+    var opciones = $("#frmRptOfertas").validate().settings;
+    return $('#frmRptOfertas').valid();
+}
+
+function loadEmpresas(empresaId) {
+    llamadaAjax("GET", "/api/empresas", null, function (err, data) {
+        if (err) return;
+        var empresas = [{ empresaId: 0, nombre: "" }].concat(data);
+        vm.posiblesEmpresas(empresas);
+        $("#cmbEmpresas").val([empresaId]).trigger('change');
+    });
+}
+
+// initAutoCliente
+// inicializa el control del cliente como un autocomplete
+var initAutoCliente = function () {
+    // incialización propiamente dicha
+    $("#txtCliente").autocomplete({
+        source: function (request, response) {
+            // call ajax
+            llamadaAjax("GET", "/api/clientes/?nombre=" + request.term, null, function (err, data) {
+                if (err) return;
+                var r = []
+                data.forEach(function (d) {
+                    var v = {
+                        value: d.nombre,
+                        id: d.clienteId
+                    };
+                    r.push(v);
+                });
+                response(r);
+            });
+        },
+        minLength: 2,
+        select: function (event, ui) {
+            vm.sclienteId(ui.item.id);
+            cambioCliente(ui.item.id);
+        }
+    });
+    // regla de validación para el control inicializado
+    jQuery.validator.addMethod("clienteNecesario", function (value, element) {
+        var r = false;
+        if (vm.sclienteId()) r = true;
+        return r;
+    }, "Debe seleccionar un cliente válido");
+};
