133fe5109189701bac7dedfe7ef73fa55db0386f
Rafael Garcia <dev.ragare@gmail.com>
2017-10-15 17:53:11 +0200

    Tab control in EmpresaDetalle.html

--- sql/2.0.49.sql
+++ sql/2.0.49.sql
@@ -1,3 +1,11 @@
 ALTER TABLE `facturas`   
   ADD COLUMN `enviadaCorreo` BOOL DEFAULT FALSE NULL AFTER `prefacturaId`,
   ADD COLUMN `ficheroCorreo` VARCHAR(255) NULL AFTER `enviadaCorreo`;
+
+CREATE TABLE `plantillas_correo_facturas`(  
+  `plaCoFacId` INT(11) NOT NULL AUTO_INCREMENT,
+  `empresaId` INT(11),
+  `plaCoFacData` BLOB,
+  PRIMARY KEY (`plaCoFacId`),
+  CONSTRAINT `ref_plaCoFa_empresas` FOREIGN KEY (`empresaId`) REFERENCES `empresas`(`empresaId`)
+);
