36ae9ef8bbe45130a48fc86d0cb88a38c124b702
rafa <rafa@myariadna.com>
2017-12-22 09:37:17 +0100

    local

--- sql/2.0.56.sql
+++ sql/2.0.56.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `proasistencia`.`proveedores`   
+CREATE TABLE `proasistencia`.`proveedores`   
   ADD COLUMN `tipoViaId` INT(11) NULL AFTER `correo`,
   ADD CONSTRAINT `proveedores_tipoVia` FOREIGN KEY (`tipoViaId`) 
   REFERENCES `proasistencia`.`tipos_via`(`tipoViaId`) 
