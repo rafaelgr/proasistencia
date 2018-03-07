8e7e42e0d78180839356ac1ff7008b183448911b
Vicent <vicent@myariadna.com>
2018-03-07 12:47:26 +0100

    funcional

--- sql/2.0.67.sql
+++ sql/2.0.67.sql
@@ -25,6 +25,8 @@ ALTER TABLE `facprove_serviciados`
 INSERT INTO tarifas VALUES(1, 10, 'Prof. tarifa 1'), (2, 10, 'Prof. tarifa 2'), (3, 10, 'Prof. tarifa 4'), (4, 10, 'Prof. tarifa 5'), 
 (5, 10, 'Prof. tarifa 6');
 
+INSERT INTO tarifas VALUES(6, 10, 'Clientes tarifa 1');
+
 INSERT INTO facprove_serviciados (facproveId, empresaId, contratoId, importe) (SELECT facproveId, empresaId, contratoId, total AS importe FROM facprove);
 
 ALTER TABLE `tarifas_lineas` DROP FOREIGN KEY `tarifaLineasTarifasFK`;
@@ -32,6 +34,7 @@ ALTER TABLE `tarifas_lineas` DROP FOREIGN KEY `tarifaLineasTarifasFK`;
 ALTER TABLE `tarifas_lineas` ADD CONSTRAINT `tarifaLineasTarifasFK` FOREIGN KEY (`tarifaId`) REFERENCES `tarifas`(`tarifaId`) ON UPDATE CASCADE ON DELETE CASCADE;
 
 CREATE TEMPORARY TABLE tarifa_temp AS (SELECT 6 AS tarifaId, articuloId, 0 AS precioUnitario FROM tarifas_lineas
- WHERE tarifaId = 1 OR tarifaId = 2 OR tarifaId = 3 OR tarifaId = 4 OR tarifaId = 5)
- INSERT INTO tarifas_lineas (tarifaId, articuloId, precioUnitario) (SELECT * FROM tarifa_temp)
+ WHERE tarifaId = 1 OR tarifaId = 2 OR tarifaId = 3 OR tarifaId = 4 OR tarifaId = 5);
+
+ INSERT INTO tarifas_lineas (tarifaId, articuloId, precioUnitario) (SELECT * FROM tarifa_temp);
 
