07eafdaffdf95eecd0b0c413536409e0cb088564
Rafael Garcia <rafa@myariadna.com>
2018-03-08 12:32:03 +0100

    Local 3

--- sql/2.0.67.sql
+++ sql/2.0.67.sql
@@ -32,13 +32,3 @@ ALTER TABLE `tarifas_lineas` DROP FOREIGN KEY `tarifaLineasTarifasFK`;
 
 ALTER TABLE `tarifas_lineas` ADD CONSTRAINT `tarifaLineasTarifasFK` FOREIGN KEY (`tarifaId`) REFERENCES `tarifas`(`tarifaId`) ON UPDATE CASCADE ON DELETE CASCADE;
 
-<<<<<<< HEAD
-DROP TABLE tarifa_temp;
-CREATE TABLE tarifa_temp AS (SELECT 6 AS tarifaId, articuloId, 0 AS precioUnitario FROM tarifas_lineas
- WHERE tarifaId = 1 OR tarifaId = 2 OR tarifaId = 3 OR tarifaId = 4 OR tarifaId = 5);
-
- INSERT INTO tarifas_lineas (tarifaId, articuloId, precioUnitario) (SELECT * FROM tarifa_temp);
-=======
- 
->>>>>>> a8d95cf1968557af9dcff6d9cf91538723900ac8
-
