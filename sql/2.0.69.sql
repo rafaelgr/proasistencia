c7b9ed80174d849383c614a85f70a4c60c5b81b0
Vicent <vicent@myariadna.com>
2018-03-13 11:52:47 +0100

    tablas OK

--- sql/2.0.69.sql
+++ sql/2.0.69.sql
@@ -1,1 +1,13 @@
 INSERT INTO tipos_mantenimiento (tipoMantenimientoId, nombre) VALUES (5, 'ARQUITECTURA');
+
+ALTER TABLE `contrato_comercial`   
+  ADD COLUMN `arqComisAgente` TINYINT(1) DEFAULT 0 NULL AFTER `finComision`,
+  ADD COLUMN `arqPorImpCliente` DECIMAL(5,2) DEFAULT 0.00 NULL AFTER `arqComisAgente`,
+  ADD COLUMN `arqPorImpClienteAgente` DECIMAL(5,2) DEFAULT 0.00 NULL AFTER `arqPorImpCliente`,
+  ADD COLUMN `arqPorCostes` DECIMAL(5,2) DEFAULT 0.00 NULL AFTER `arqPorImpClienteAgente`,
+  ADD COLUMN `arqCostes` TINYINT(1) DEFAULT 0 NULL AFTER `arqPorCostes`,
+  ADD COLUMN `arqJefeObra` TINYINT(1) DEFAULT 0 NULL AFTER `arqCostes`,
+  ADD COLUMN `arqOficinaTecnica` TINYINT(1) DEFAULT 0 NULL AFTER `arqJefeObra`,
+  ADD COLUMN `arqAsesorTecnico` TINYINT(1) DEFAULT 0 NULL AFTER `arqOficinaTecnica`,
+  ADD COLUMN `arqComercial` TINYINT(1) DEFAULT 0 NULL AFTER `arqAsesorTecnico`,
+  ADD COLUMN `arqComision` DECIMAL(5,2) DEFAULT 0.00 NULL AFTER `arqComercial`;
