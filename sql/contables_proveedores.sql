8b05ff972a8dbb0eb79f0419ccfa78376bcb1346
Vicent <vicent@myariadna.com>
2018-04-12 12:59:32 +0200

    sql OK

---
+++ sql/contables_proveedores.sql
@@ -0,0 +1,45 @@
+INSERT IGNORE INTO ariconta11.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+
+INSERT IGNORE INTO ariconta12.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta13.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta14.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta15.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta16.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta17.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta18.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
+
+INSERT IGNORE INTO ariconta19.cuentas 
+(codmacta,nommacta,apudirec,model347,razosoci,dirdatos,codposta,despobla,desprovi,nifdatos,maidatos,iban,forpa) 
+SELECT cuentaContable,nombre,'S',1,nombre,direccion,codPostal,poblacion,provincia,nif,correo,IBAN,codigo 
+FROM proveedores;
