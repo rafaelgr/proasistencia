e689673daec205c0b3ba2b35dc3e793928981975
unknown <vicent@myariadna.com>
2018-10-01 11:16:29 +0200

    version y sql OK

---
+++ sql/2.1.15.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `facprove_retenciones`(  
+  `facproveRetencionId` INT(11) NOT NULL AUTO_INCREMENT,
+  `facproveId` INT(11) NOT NULL,
+  `baseRetencion` DECIMAL(12,0),
+  `porcentajeRetencion` DECIMAL(4,2),
+  `importeRetencion` DECIMAL(12,2),
+   KEY(`facproveRetencionId`),
+  CONSTRAINT `fecproveRetencion_facproveFK` FOREIGN KEY (`facproveId`) REFERENCES `facprove`(`facproveId`) ON UPDATE CASCADE ON DELETE NO ACTION
+);
