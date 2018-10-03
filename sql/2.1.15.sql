ace9b52793cd311e10303e44d28da345e855db7b
unknown <vicent@myariadna.com>
2018-10-03 13:05:17 +0200

    llamada api a tipos OK

--- sql/2.1.15.sql
+++ sql/2.1.15.sql
@@ -4,12 +4,20 @@ CREATE TABLE `facprove_retenciones`(
   `baseRetencion` DECIMAL(12,2),
   `porcentajeRetencion` DECIMAL(4,2),
   `importeRetencion` DECIMAL(12,2),
+  `codigoRetencion` SMALLINT(11),
+   `cuentaRetencion` VARCHAR(10),
    KEY(`facproveRetencionId`),
   CONSTRAINT `fecproveRetencion_facproveFK` FOREIGN KEY (`facproveId`) REFERENCES `facprove`(`facproveId`) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 
+
 ALTER TABLE `facprove_lineas`   
   ADD COLUMN `porcentajeRetencion` DECIMAL(4,2) DEFAULT 0  AFTER `capituloLinea`,
-  ADD COLUMN `importeRetencion` DECIMAL(12,2) DEFAULT 0 AFTER `porcentajeRetencion`;
+  ADD COLUMN `importeRetencion` DECIMAL(12,2) DEFAULT 0 AFTER `porcentajeRetencion`,
+  ADD COLUMN `codigoRetencion` SMALLINT(11) NULL AFTER `importeRetencion`,
+  ADD COLUMN `cuentaRetencion` VARCHAR(10) NULL AFTER `codigoRetencion`;
+
+  
+
 
