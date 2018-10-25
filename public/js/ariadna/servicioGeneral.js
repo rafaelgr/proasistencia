a967e770136068e1012d733cdfd41d4997c5f339
unknown <vicent@myariadna.com>
2018-10-25 11:34:37 +0200

    modificacion enlace OK

---
+++ public/js/ariadna/servicioGeneral.js
@@ -0,0 +1,260 @@
+/*-------------------------------------------------------------------------- 
+usuarioGeneral.js
+Funciones js par la página UsuarioGeneral.html
+
+---------------------------------------------------------------------------*/
+var responsiveHelper_dt_basic = undefined;
+var responsiveHelper_datatable_fixed_column = undefined;
+var responsiveHelper_datatable_col_reorder = undefined;
+var responsiveHelper_datatable_tabletools = undefined;
+
+var dataUsuarios;
+var usuarioId;
+
+var breakpointDefinition = {
+    tablet: 1024,
+    phone: 480
+};
+
+
+function initForm() {
+    comprobarLogin();
+    // de smart admin
+    pageSetUp();
+    getVersionFooter();
+    //
+    $('#btnBuscar').click(buscarUsuarios());
+    $('#btnAlta').click(crearUsuario());
+    $('#frmBuscar').submit(function () {
+        return false
+    });
+    //$('#txtBuscar').keypress(function (e) {
+    //    if (e.keyCode == 13)
+    //        buscarUsuarios();
+    //});
+    //
+    initTablaUsuarios();
+    // comprobamos parámetros
+    usuarioId = gup('UsuarioId');
+    if (usuarioId !== '') {
+        // cargar la tabla con un único valor que es el que corresponde.
+        var data = {
+            id: usuarioId
+        }
+        // hay que buscar ese elemento en concreto
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/usuarios/" + usuarioId,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                var data2 = [data];
+                loadTablaUsuarios(data2);
+            },
+                            error: function (err) {
+                    mensErrorAjax(err);
+                    // si hay algo más que hacer lo haremos aquí.
+                }
+        });
+    }else {
+        buscarTodos();
+    }
+}
+
+function initTablaUsuarios() {
+    tablaCarro = $('#dt_usuario').dataTable({
+        autoWidth: true,
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper_dt_basic) {
+                responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#dt_usuario'), breakpointDefinition);
+            }
+        },
+        rowCallback: function (nRow) {
+            responsiveHelper_dt_basic.createExpandIcon(nRow);
+        },
+        drawCallback: function (oSettings) {
+            responsiveHelper_dt_basic.respond();
+        },
+        language: {
+            processing: "Procesando...",
+            info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            infoFiltered: "(filtrado de un total de _MAX_ registros)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron resultados",
+            emptyTable: "Ningún dato disponible en esta tabla",
+            paginate: {
+                first: "Primero",
+                previous: "Anterior",
+                next: "Siguiente",
+                last: "Último"
+            },
+            aria: {
+                sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                sortDescending: ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        data: dataUsuarios,
+        columns: [{
+            data: "nombre"
+        }, {
+            data: "login"
+        }, {
+            data: "email"
+        }, {
+            data: "usuarioId",
+            render: function (data, type, row) {
+                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='deleteUsuario(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success btn-lg' onclick='editUsuario(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
+                return html;
+            }
+        }]
+    });
+}
+
+function datosOK() {
+    
+    $('#frmBuscar').validate({
+        rules: {
+            txtBuscar: { required: true },
+        },
+        // Messages for form validation
+        messages: {
+            txtBuscar: {
+                required: 'Introduzca el texto a buscar'
+            }
+        },
+        // Do not change code below
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+    return $('#frmBuscar').valid();
+}
+
+function loadTablaUsuarios(data) {
+    var dt = $('#dt_usuario').dataTable();
+    if (data !== null && data.length === 0) {
+        mostrarMensajeSmart('No se han encontrado registros');
+        $("#tbUsuario").hide();
+    } else {
+        dt.fnClearTable();
+        dt.fnAddData(data);
+        dt.fnDraw();
+        $("#tbUsuario").show();
+    }
+}
+
+function buscarUsuarios() {
+    var mf = function () {
+        if (!datosOK()) {
+            return;
+        }
+        // obtener el n.serie del certificado para la firma.
+        var aBuscar = $('#txtBuscar').val();
+        // enviar la consulta por la red (AJAX)
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/usuarios/?nombre=" + aBuscar,
+            dataType: "json",
+            contentType: "application/json",
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadTablaUsuarios(data);
+            },
+                            error: function (err) {
+                    mensErrorAjax(err);
+                    // si hay algo más que hacer lo haremos aquí.
+                }
+        });
+    };
+    return mf;
+}
+
+function crearUsuario() {
+    var mf = function () {
+        var url = "UsuarioDetalle.html?UsuarioId=0";
+        window.open(url, '_self');
+    };
+    return mf;
+}
+
+function deleteUsuario(id) {
+    // mensaje de confirmación
+    var mens = "¿Realmente desea borrar este registro?";
+    $.SmartMessageBox({
+        title: "<i class='fa fa-info'></i> Mensaje",
+        content: mens,
+        buttons: '[Aceptar][Cancelar]'
+    }, function (ButtonPressed) {
+        if (ButtonPressed === "Aceptar") {
+            var data = {
+                usuarioId: id
+            };
+            $.ajax({
+                type: "DELETE",
+                url: myconfig.apiUrl + "/api/usuarios/" + id,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+                    var fn = buscarUsuarios();
+                    fn();
+                },
+                                error: function (err) {
+                    mensErrorAjax(err);
+                    // si hay algo más que hacer lo haremos aquí.
+                }
+            });
+        }
+        if (ButtonPressed === "Cancelar") {
+            // no hacemos nada (no quiere borrar)
+        }
+    });
+}
+
+function editUsuario(id) {
+    // hay que abrir la página de detalle de usuario
+    // pasando en la url ese ID
+    var url = "UsuarioDetalle.html?UsuarioId=" + id;
+    window.open(url, '_self');
+}
+
+
+function buscarUsuarios() {
+    var mf = function () {
+        if (!datosOK()) {
+            return;
+        }
+        // obtener el n.serie del certificado para la firma.
+        var aBuscar = $('#txtBuscar').val();
+        // enviar la consulta por la red (AJAX)
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/usuarios/?nombre=" + aBuscar,
+            dataType: "json",
+            contentType: "application/json",
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadTablaUsuarios(data);
+            },
+                            error: function (err) {
+                    mensErrorAjax(err);
+                    // si hay algo más que hacer lo haremos aquí.
+                }
+        });
+    };
+    return mf;
+}
+
+buscarTodos = function(){
+    var url = myconfig.apiUrl + "/api/usuarios/?nombre=*";
+    llamadaAjax("GET",url, null, function(err, data){
+        loadTablaUsuarios(data);
+    });
+}
