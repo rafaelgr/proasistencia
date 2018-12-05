f2133bc7a5480d48f6adb98b462d91b52f8244d8
unknown <vicent@myariadna.com>
2018-12-05 12:48:51 +0100

    actualizacion articulos OK

--- sql/2.2.1.sql
+++ sql/2.2.1.sql
@@ -1,4 +1,21 @@
 /*EJECUATAR EL FICHERO tmp_cuentas
 Y DESPUES EJECUTAR EL FICHERO updateContas.sql en BBDD de proasistencia*/
 
+/*Para asignar los articulos con su grupo correspondiente según cod. reparación ejecutar actualiza_articulos_codRep.sql*/
 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
