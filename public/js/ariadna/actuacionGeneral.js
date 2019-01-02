a368d8285cd5a9c1e81f0a57232a9c87a78cf215
unknown <vicent@myariadna.com>
2019-01-02 08:48:18 +0100

    cambios  28/12/2018

--- public/js/ariadna/actuacionGeneral.js
+++ public/js/ariadna/actuacionGeneral.js
@@ -125,8 +125,8 @@ function initTablaActuaciones() {
         },  {
             data: "actuacionId",
             render: function (data, type, row) {
-                var bt1 = "<button class='btn btn-circle btn-danger' onclick='deleteFactura(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
-                var bt2 = "<button class='btn btn-circle btn-success' onclick='editFactura(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var bt1 = "<button class='btn btn-circle btn-danger' onclick='deleteActuacion(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success' onclick='editActuacion(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
                 var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
                 return html;
             }
