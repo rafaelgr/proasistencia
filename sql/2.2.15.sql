d5749083a9d98182a68492df48282c40fc048992
vicent <vicent@myariadna.com>
2019-04-04 13:32:59 +0200

    final dia

--- sql/2.2.15.sql
+++ sql/2.2.15.sql
@@ -1,2 +1,8 @@
 ALTER TABLE `estados_partes`   
   ADD COLUMN `colorFondo` VARCHAR(255) DEFAULT '#FFFFFF' NULL AFTER `nombre`;
+
+
+ALTER TABLE `proasistencia`.`servicios`   
+  ADD COLUMN `tipoProfesionalId` INT(11) NULL COMMENT 'id del tipo de profesional del servicio' AFTER `clienteId`, 
+  ADD  KEY `ref_servicio_tipoProf` (`tipoProfesionalId`),
+  ADD CONSTRAINT `ref_servicio_tipoProf` FOREIGN KEY (`tipoProfesionalId`) REFERENCES `proasistencia`.`tipos_profesionales`(`tipoProfesionalId`) ON UPDATE CASCADE ON DELETE NO ACTION;
