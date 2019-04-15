7c4425b157f499308f62d27e19d939685cebe726
vicent <vicent@myariadna.com>
2019-04-15 13:24:13 +0200

    primeros cambios

---
+++ sql/2.2.18.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `proasistencia`.`servicios`   
+  CHANGE `agenteId` `agenteId` INT(11) NOT NULL COMMENT 'Clave referencia del agente responsable de la solicitud'  FIRST,
+  ADD COLUMN `usuarioId` INT(11) NULL AFTER `clienteId`,
+  CHANGE `servicioId` `servicioId` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria que identifica a un servicio,'  AFTER `usuarioId`;
