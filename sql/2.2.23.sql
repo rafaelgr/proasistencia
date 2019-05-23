0efe83ae9e4c258194c8822c9f06b3f12352b1e3
vicent <vicent@myariadna.com>
2019-05-23 13:01:59 +0200

    primeros cambios

---
+++ sql/2.2.23.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `departamentos` (
+  `departamentoId` int(11) NOT NULL,
+  `nombre` varchar(255) default NULL,
+  PRIMARY KEY  (`departamentoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `departamentos` */
+
+insert  into `departamentos`(`departamentoId`,`nombre`) values (1,'MANTENIMIENTO');
+insert  into `departamentos`(`departamentoId`,`nombre`) values (2,'SEGUROS');
+insert  into `departamentos`(`departamentoId`,`nombre`) values (3,'ALQUILERES');
+insert  into `departamentos`(`departamentoId`,`nombre`) values (4,'FINCAS');
+insert  into `departamentos`(`departamentoId`,`nombre`) values (5,'ARQUITECTURA');
+insert  into `departamentos`(`departamentoId`,`nombre`) values (6,'ADMINISTRACIÓN');
+insert  into `departamentos`(`departamentoId`,`nombre`) values (7,'REPARACIONES');
