2ce18ece1f0afec9edb128d7264ad6ab7cdb3226
vicent <vicent@myariadna.com>
2019-06-07 13:41:11 +0200

    final dia

--- sql/2.2.25.sql
+++ sql/2.2.25.sql
@@ -1,2 +1,14 @@
 ALTER TABLE `facturas`   
   ADD COLUMN `noCalculadora` TINYINT(1) DEFAULT 0 NULL AFTER `departamentoId`;
+
+  UPDATE partes SET `forma_pago_cliente` = NULL;
+
+  ALTER TABLE `partes`   
+  CHANGE `forma_pago_cliente` `forma_pago_cliente` INT(11) NULL;
+
+  ALTER TABLE `partes`   
+  ADD COLUMN `formaPagoProfesionalId` INT(11) NULL AFTER `fecha_pago_profesional`;
+
+
+ALTER TABLE `partes`   
+  CHANGE `forma_pago_cliente` `formaPagoClienteId` INT(11) NULL;
