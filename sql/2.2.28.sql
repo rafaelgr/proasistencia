e90798382772c6c910aebc1b1f5612ed038d8ee3
vicent <vicent@myariadna.com>
2019-06-20 13:39:01 +0200

    final dia

--- sql/2.2.28.sql
+++ sql/2.2.28.sql
@@ -1,3 +1,8 @@
 ALTER TABLE `partes`   
   ADD COLUMN `FactPropiaCli` TINYINT(1) DEFAULT 0 NULL AFTER `formaPagoClienteId`,
   ADD COLUMN `FactPropiaPro` TINYINT(1) DEFAULT 0 NULL AFTER `formaPagoProfesionalId`;
+
+  ALTER TABLE `partes`   
+  ADD COLUMN `facturaId` INT(11) NULL AFTER `enviar_otro_profesional`,
+  ADD CONSTRAINT `ref_parte_factura` FOREIGN KEY (`facturaId`) REFERENCES `proasistencia`.`facturas`(`facturaId`);
+
