28622b2ad8200facd34c590f47ddfbd3b0dc8854
vicent <vicent@myariadna.com>
2019-07-01 13:21:09 +0200

    fallito

--- sql/2.2.29.sql
+++ sql/2.2.29.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `partes`   
   ADD COLUMN `numero` INT(11) NULL AFTER `facturaId`;
  
- LTER TABLE `partes`   
+ ALTER TABLE `partes`   
   ADD COLUMN `ano` VARCHAR(255) NULL AFTER `numero`;
 
   UPDATE partes set ano = '2019'
