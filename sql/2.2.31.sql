4ae5a948ae7fc96f7682c651fe91555ea0e5ec6e
vicent <vicent@myariadna.com>
2019-07-03 10:22:03 +0200

    provisional

---
+++ sql/2.2.31.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `partes`  
+  ADD CONSTRAINT `ref_parte_facturas` FOREIGN KEY (`facturaId`) REFERENCES `facturas`(`facturaId`) ON DELETE SET NULL;
+
+  ALTER TABLE `partes`  
+  ADD CONSTRAINT `ref_parte_facprove` FOREIGN KEY (`facproveId`) REFERENCES `facprove`(`facproveId`) ON DELETE SET NULL;
+
