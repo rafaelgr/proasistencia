052d73a9ebb23a37521656f9d15b00d576de854f
vicent <vicent@myariadna.com>
2019-07-03 11:44:19 +0200

    todo OK

--- sql/2.2.30.sql
+++ sql/2.2.30.sql
@@ -3,11 +3,6 @@ ALTER TABLE `partes`
 
 ALTER TABLE `prefacturas`   
   ADD COLUMN `noCalculadora` TINYINT(1) DEFAULT 0 NULL AFTER `departamentoId`;
-<<<<<<< HEAD
-
-  ALTER TABLE `partes`  
-=======
  ALTER TABLE `partes`  
->>>>>>> develop
   ADD CONSTRAINT `ref_parte_prefacturas` FOREIGN KEY (`prefacturaId`) REFERENCES `prefacturas`(`prefacturaId`) ON DELETE SET NULL;
 
