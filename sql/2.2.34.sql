e33675aad58525f41904171e41698fdec9162789
vicent <vicent@myariadna.com>
2019-07-19 13:44:14 +0200

    final dia

--- sql/2.2.34.sql
+++ sql/2.2.34.sql
@@ -4,4 +4,11 @@ ALTER TABLE `partes_lineas`
   ADD CONSTRAINT `lineas_parte_factura` FOREIGN KEY (`facturaLineaId`) REFERENCES `facturas_lineas`(`facturaLineaId`) ON UPDATE CASCADE ON DELETE SET NULL,
   ADD CONSTRAINT `lineas_parte_facprove` FOREIGN KEY (`facproveLineaId`) REFERENCES `facprove_lineas`(`facproveLineaId`) ON UPDATE CASCADE  ON DELETE SET NULL;
 
+ALTER TABLE `partes`   
+  ADD COLUMN `empresaParteId` INT(11) NULL AFTER `facproveId`;
+
+  ALTER TABLE `partes`  
+  ADD CONSTRAINT `ref_parte_empresa` FOREIGN KEY (`empresaParteId`) REFERENCES `empresas`(`empresaId`);
+
+
 
