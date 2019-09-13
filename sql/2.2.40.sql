1325b98f253e0d5c251c7274a93a44dc15fa5475
vicent <vicent@myariadna.com>
2019-09-13 13:27:57 +0200

    correccion sql

--- sql/2.2.40.sql
+++ sql/2.2.40.sql
@@ -1,11 +1,11 @@
-ALTER TABLE `proasistencia`.`clientes`   
+ALTER TABLE `clientes`   
   ADD COLUMN `colaboradorId` INT(11) NULL AFTER `tarifaId`,
-  ADD CONSTRAINT `ref_cliente_colaborador` FOREIGN KEY (`colaboradorId`) REFERENCES `proasistencia`.`comerciales`(`comercialId`);
+  ADD CONSTRAINT `ref_cliente_colaborador` FOREIGN KEY (`colaboradorId`) REFERENCES `comerciales`(`comercialId`);
 
 
 UPDATE clientes AS cli
 INNER JOIN clientes_comisionistas AS cc ON cc.clienteId = cli.clienteId
-SET cli.colaboradorId = cc.comercialId 
+SET cli.colaboradorId = cc.comercialId;
 
 DROP TABLE IF EXISTS `agentes_colaboradores`;
 
