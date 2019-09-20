7a6fcffea8d322a0d89d1943c1617bf6339d0a57
vicent <vicent@myariadna.com>
2019-09-20 13:44:28 +0200

    final dia

--- sql/2.2.42.sql
+++ sql/2.2.42.sql
@@ -2,3 +2,7 @@ ALTER TABLE `clientes`
   ADD COLUMN `emailFacturas` VARCHAR(255) NULL AFTER `limiteCredito`;
   
 UPDATE clientes SET `emailFacturas` = `email`;
+
+LTER TABLE `proveedores`   
+  CHANGE `fianzaAcumulada` `fianzaAcumulada` DECIMAL(10,2) DEFAULT 0 NULL,
+  CHANGE `retencionFianza` `retencionFianza` DECIMAL(10,2) DEFAULT 0 NULL;
