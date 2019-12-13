23e641cebd603f3005fbc842835415ac1a82b4d9
vicent <vicent@myariadna.com>
2019-12-13 10:30:46 +0100

    todo OK

--- sql/2.2.58.sql
+++ sql/2.2.58.sql
@@ -2,3 +2,4 @@ ALTER TABLE `proveedores`
   ADD COLUMN `tipoIvaId` INT(11) NULL AFTER `tarifaId`,
   ADD CONSTRAINT `proveedores_tiposIva` FOREIGN KEY (`tipoIvaId`) REFERENCES `tipos_iva`(`tipoIvaId`) ON UPDATE CASCADE ON DELETE SET NULL;
 
+UPDATE clientes SET tipoIvaId = 5, limiteCredito = 400;
