38487726c5c0d31306a7a4055ce1f99698ded42e
vicent <vicent@myariadna.com>
2019-12-30 13:07:46 +0100

    cambio version

---
+++ sql/2.2.64.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `ofertas_lineas`   
+  ADD COLUMN `proveedorId` INT(11) NULL AFTER `capituloLinea`,
+  ADD COLUMN `importeProveedor` DECIMAL(10,2) NULL AFTER `proveedorId`,
+  ADD COLUMN `totalLineaProveedor` DECIMAL(12,2) NULL AFTER `importeProveedor`,
+  ADD CONSTRAINT `ofl_proveedores` FOREIGN KEY (`proveedorId`) REFERENCES `proveedores`(`proveedorId`);
