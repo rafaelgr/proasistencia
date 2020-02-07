e284387a6599e5d10ad698f99ec2d0cb3f5548ee
vicent <vicent@myariadna.com>
2020-02-07 13:38:07 +0100

    final dia

--- sql/2.2.69.sql
+++ sql/2.2.69.sql
@@ -2,3 +2,8 @@ ALTER TABLE `articulos`
   ADD COLUMN `coste` DECIMAL(10,2) DEFAULT 0 NULL AFTER `varios`,
   ADD COLUMN `porcentaje` DECIMAL(10,2) DEFAULT 80 NULL AFTER `coste`,
   ADD COLUMN `precioVenta` DECIMAL(10,2) DEFAULT 0 NULL AFTER `porcentaje`;
+
+  ALTER TABLE `tipos_proveedor`   
+  ADD COLUMN `inicioCuentaFianza` INT(11) NULL AFTER `inicioCuenta`;
+
+  UPDATE tipos_proveedor SET inicioCuentaFianza = 180;
