9cd10b3e4422e5d7131842ae0c30fdb889f2fd11
vicent <vicent@myariadna.com>
2020-04-01 12:43:22 +0200

    todo OK

--- sql/2.3.6.sql
+++ sql/2.3.6.sql
@@ -2,4 +2,4 @@ ALTER TABLE `contratos`
   CHANGE `clienteId` `clienteId` INT(11) NULL COMMENT 'Cliente final al que va dirigida el contrato';
 
 ALTER TABLE `departamentos`   
-  ADD COLUMN `usaCliente` TINYINT(1) DEFAULT 1 NULL AFTER `usaCalculadora`;
+  ADD COLUMN `usaContrato` TINYINT(1) DEFAULT 1 NULL AFTER `usaCalculadora`;
