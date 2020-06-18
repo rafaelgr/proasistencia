0a271b788ed3da7058af1acc28c223fa8d180bac
vicent <vicent@myariadna.com>
2020-06-18 10:19:17 +0200

    codigo socket comentado

--- public/js/ariadna/contaEGeneral.js
+++ public/js/ariadna/contaEGeneral.js
@@ -95,7 +95,7 @@ function initForm() {
     // comprobamos parámetros
     facturaId = gup('FacturaId');
     //
-    var socket = io.connect('/');
+    /* var socket = io.connect('/');
     socket.on('message', function (data) {
         alert(data);
     });
@@ -113,7 +113,7 @@ function initForm() {
         if (percentComplete === 100) {
             $('.progress-bar').html('Proceso terminado');
         }
-    });
+    }); */
 
     //Evento de marcar/desmarcar todos los checks
     $('#checkMain').click(
