3ce2516edd2d50bf3e9ecee1396d02ef9bc34288
vicent <vicent@myariadna.com>
2020-07-16 11:43:27 +0200

    cambiado

--- sql/2.3.30.sql
+++ sql/2.3.30.sql
@@ -5,7 +5,7 @@ ALTER TABLE `parametros`
   ADD COLUMN `bucket` VARCHAR(255) NULL AFTER `cuentaretencion`,
   ADD COLUMN `bucket_region` VARCHAR(255) NULL AFTER `bucket`,
   ADD COLUMN `bucket_folder` VARCHAR(255) NULL AFTER `bucket_region`,
-  ADD COLUMN `indentity_pool` VARCHAR(255) NULL AFTER `bucket_folder`;
+  ADD COLUMN `identity_pool` VARCHAR(255) NULL AFTER `bucket_folder`;
   ADD COLUMN `raiz_url` VARCHAR(255) NULL AFTER `indentity_pool`;
 
  
