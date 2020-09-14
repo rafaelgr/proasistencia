0d270ef1eebc38b1dd8b23ce6d41f4e6dd8ec902
vicent <vicent@myariadna.com>
2020-09-14 13:39:19 +0200

    final dia

--- sql/2.3.42.sql
+++ sql/2.3.42.sql
@@ -1,2 +1,10 @@
 ALTER TABLE `empresas`   
   ADD COLUMN `infFacCliObr` VARCHAR(255) NULL AFTER `serieFacRep`;
+
+  update empresas set  infFacCliObr = 'factcli_obras_reabita' where empresaId = 7;
+
+   update empresas set  infFacCliObr = 'factcli_obras_fondo' where empresaId = 3;
+
+    update empresas set  infFacCliObr = 'factcli_obras_proas' where empresaId = 2;
+
+
