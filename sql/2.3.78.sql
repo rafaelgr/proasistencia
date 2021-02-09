4480c5c50616324f5617dc587bca386973e87308
vicent <vicent@myariadna.com>
2021-02-09 13:42:22 +0100

    final dia

--- sql/2.3.78.sql
+++ sql/2.3.78.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `usuarios`   
-  ADD COLUMN `puedeVisualizar` TINYINT(1) DEFAULT 1 NULL AFTER `departamentoTrabajo`;
+  ADD COLUMN `puedeVisualizar` TINYINT(1) DEFAULT 0 NULL AFTER `departamentoTrabajo`;
