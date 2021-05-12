cd428acd4c0b43ecaddf241ee065ea8427abb870
vicent <vicent@myariadna.com>
2021-05-12 11:56:43 +0200

    final dia

--- sql/2.3.93.sql
+++ sql/2.3.93.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `liquidacion_comercial`   
   ADD COLUMN `ascContratoId` INT(11) NULL AFTER `hFecha`;
+
+
+ALTER TABLE `liquidacion_comercial_obras`   
+  ADD COLUMN `ascContratoId` INT(11) NULL AFTER `contratoId`;
