159bf3ce2eb36cca2b089f3ce989e49907cddc48
vicent <vicent@myariadna.com>
2021-05-24 14:03:54 +0200

    final dia

--- sql/2.3.94.sql
+++ sql/2.3.94.sql
@@ -5,3 +5,10 @@ ALTER TABLE `proveedores`
 UPDATE proveedores SET activa = 0 WHERE NOT fechaBaja IS NULL;
 
 UPDATE proveedores SET activa = 1 WHERE fechaBaja IS NULL;
+
+ALTER TABLE `liquidacion_comercial_obras`   
+  ADD COLUMN `certificacionFinal` DECIMAL(12,2) NULL AFTER `importeObra`;
+
+  ALTER TABLE `liquidacion_comercial_obras`   
+  ADD COLUMN `baseAdicional` DECIMAL(12,2) NULL AFTER `baseAnterior`;
+
