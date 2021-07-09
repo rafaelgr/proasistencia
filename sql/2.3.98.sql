1fa46dad1e521bbbbe76e447f2e3255a0a2b0b99
vicent <vicent@myariadna.com>
2021-07-09 08:34:46 +0200

    sql añadida

--- sql/2.3.98.sql
+++ sql/2.3.98.sql
@@ -1,3 +1,8 @@
 ALTER TABLE `proasistencia`.`proveedores`   
   ADD COLUMN `login` VARCHAR(255) NULL AFTER `activa`,
   ADD COLUMN `password` VARCHAR(255) NULL AFTER `login`;
+
+  ALTER TABLE `servicios`   
+  ADD COLUMN `proveedorId` INT(11) NULL AFTER `observacionesOperador`,
+  ADD CONSTRAINT `ref_servicio_proveedor` FOREIGN KEY (`proveedorId`) REFERENCES `proveedores`(`proveedorId`) ON UPDATE CASCADE ON DELETE NO ACTION;
+
