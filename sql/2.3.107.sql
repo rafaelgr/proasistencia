08e8ea6d0b2cd5bc26011aa33197cbd46ef78dd3
vicent <vicent@myariadna.com>
2022-02-16 09:40:14 +0100

    sql cambiada

--- sql/2.3.107.sql
+++ sql/2.3.107.sql
@@ -2,3 +2,12 @@ ALTER TABLE `proveedores`
   DROP COLUMN `login`, 
   DROP COLUMN `password`, 
   DROP COLUMN `playerId`;
+
+
+  ALTER TABLE `partes`   
+  ADD COLUMN `notasWeb` VARCHAR(255) NULL AFTER `pdf`;
+
+  ALTER TABLE `partes`   
+  CHANGE `notasWeb` `notasWeb` TEXT CHARSET utf8 COLLATE utf8_general_ci NULL;
+
+
