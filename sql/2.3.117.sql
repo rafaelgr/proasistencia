e77abc2df0a40a642eecf3ef8359906388503f11
vicent <vicent@myariadna.com>
2022-06-06 09:48:43 +0200

    provisional

--- sql/2.3.117.sql
+++ sql/2.3.117.sql
@@ -1,4 +1,12 @@
-ALTER TABLE `proasistencia`.`mensajes`   
+ALTER TABLE `mensajes`   
   ADD COLUMN `proveedorId` INT(11) NULL AFTER `parteId`, 
   ADD  INDEX `menasjes_proveedoresFK` (`proveedorId`),
   ADD CONSTRAINT `menasjes_proveedoresFK` FOREIGN KEY (`proveedorId`) REFERENCES `proasistencia`.`proveedores`(`proveedorId`);
+
+
+ALTER TABLE `mensajes`   
+  ADD COLUMN `proveedorUsuarioPushId` INT(11) NULL AFTER `proveedorId`;
+
+   ALTER TABLE `mensajes` ADD CONSTRAINT `mensajes_proveedorPushFK` 
+   FOREIGN KEY (`proveedorUsuarioPushId`) REFERENCES `proasistencia`.`proveedor_usuariospush`(`proveedorUsuarioPushId`); 
+
