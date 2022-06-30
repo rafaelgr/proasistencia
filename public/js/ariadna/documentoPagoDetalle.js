b4efe8db5a0e0998ae177504601c017b6950224a
vicent <vicent@myariadna.com>
2022-06-30 13:28:54 +0200

    final dia

--- public/js/ariadna/documentoPagoDetalle.js
+++ public/js/ariadna/documentoPagoDetalle.js
@@ -3,20 +3,27 @@ documentopagoDetalle.js
 Funciones js par la página DocumentoPagoDetalle.html
 ---------------------------------------------------------------------------*/
 var documentoPagoId = 0;
+var usuario;
 
-var posiblesNiveles = [{
-    id: 0,
-    nombre: "DocumentoPago"
-}, {
-    id: 1,
-    nombre: "Jefe de Equipo"
-}, {
-    id: 2,
-    nombre: "Vigilante"
-}];
+var responsiveHelper_dt_basic = undefined;
+var responsiveHelper_datatable_fixed_column = undefined;
+var responsiveHelper_datatable_col_reorder = undefined;
+var responsiveHelper_datatable_tabletools = undefined;
+
+var breakpointDefinition = {
+    tablet: 1024,
+    phone: 480
+};
+
+
+var dataFacturasAsociadas;
+var dataAsociarFacturas;
+
+
 
 function initForm() {
     comprobarLogin();
+    usuario = recuperarUsuario();
     // de smart admin
     pageSetUp();
     // /
@@ -25,10 +32,42 @@ function initForm() {
     ko.applyBindings(vm);
     // asignación de eventos al clic
     $("#btnAceptar").click(aceptar());
+    $("#btnbuscarAsociarFacturas").click(aceptarBuscarAsociarFacturas());
+    
     $("#btnSalir").click(salir());
     $("#frmDocumentoPago").submit(function() {
         return false;
     });
+
+    $("#frmAsociarFacturas").submit(function() {
+        return false;
+    });
+
+    $("#frmNuevaFacturaAsociada").submit(function() {
+        return false;
+    });
+
+    $("#cmbEmpresas").select2(select2Spanish());
+    loadEmpresas();
+
+    $("#cmbDepartamentos").select2(select2Spanish());
+    loadDeparta();
+
+    //Evento de marcar/desmarcar todos los checks
+    $('#checkMain').click(
+        function(e){
+            if($('#checkMain').prop('checked')) {
+                $('.checkAll').prop('checked', true);
+                updateAll(true);
+            } else {
+                $('.checkAll').prop('checked', false);
+                updateAll(false);
+            }
+        }
+    );
+
+    initTablaFacturasAsociadas();
+    initTablaAsociarFacturas();
 
 
 
@@ -57,7 +96,6 @@ function initForm() {
         // se trata de un alta ponemos el id a cero para indicarlo.
         vm.documentopagoId(0);
         $("#cmbTiposProfesional").select2(select2Spanish());
-        loadTiposProfesionales();
     }
 }
 
@@ -66,15 +104,28 @@ function admData() {
     self.documentopagoId = ko.observable();
     self.nombre = ko.observable();
     self.numero = ko.observable();
-    //
-    //
+    self.dFecha = ko.observable();
+    self.hFecha = ko.observable();
     self.facproveId = ko.observable();
+    //
+    self.departamentoId = ko.observable();
+    self.sdepartamentoId = ko.observable();
+    //
+    self.posiblesDepartamentos = ko.observableArray([]);
+    self.elegidosDepartamentos = ko.observableArray([]);
+    //
+    self.empresaId = ko.observable();
+    self.sempresaId = ko.observable();
+    //
+    self.posiblesEmpresas = ko.observableArray([]);
+    self.elegidosEmpresas = ko.observableArray([]);
 }
 
 function loadData(data) {
     vm.documentopagoId(data.documentopagoId);
     vm.nombre(data.nombre);
     vm.numero(data.numero);
+    loadTablaFacturas(data.facturas)
 }
 
 function datosOK() {
@@ -173,6 +224,73 @@ function salir() {
     return mf;
 }
 
+function initTablaFacturasAsociadas() {
+    tablaCarro = $('#dt_FacturasAsociadas').DataTable({
+        autoWidth: true,
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper_dt_basic) {
+                responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#dt_FacturasAsociadas'), breakpointDefinition);
+            }
+        },
+        rowCallback: function (nRow) {
+            responsiveHelper_dt_basic.createExpandIcon(nRow);
+        },
+        drawCallback: function (oSettings) {
+            responsiveHelper_dt_basic.respond();
+        },
+        language: {
+            processing: "Procesando...",
+            info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            infoFiltered: "(filtrado de un total de _MAX_ registros)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron resultados",
+            emptyTable: "Ningún dato disponible en esta tabla",
+            paginate: {
+                first: "Primero",
+                previous: "Anterior",
+                next: "Siguiente",
+                last: "Último"
+            },
+            aria: {
+                sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                sortDescending: ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        data: dataFacturasAsociadas,
+        columns: [{
+            data: "numeroFacturaProveedor"
+        }, {
+            data: "ref"
+        }, {
+            data: "fecha"
+        },{
+            data: "total",
+            className: "text-right",
+            render: function (data, type, row) {
+                return numeral(data).format('0,0.00');
+            }
+        }, {
+            data: "totalConIva",
+            className: "text-right",
+            render: function (data, type, row) {
+                return numeral(data).format('0,0.00');
+            }
+        }, {
+            data: "facproveId",
+            render: function (data, type, row) {
+                var html = "";
+                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='deleteFacturaLinea(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success btn-lg' data-toggle='modal' data-target='#modalLinea' onclick='editFacturaLinea(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
+                return html;
+            }
+        }]
+    });
+}
+
 
 function loadTablaFacturas(data) {
     var dt = $('#dt_FacturasAsociadas').dataTable();
@@ -182,4 +300,244 @@ function loadTablaFacturas(data) {
     dt.fnClearTable();
     dt.fnAddData(data);
     dt.fnDraw();
+}
+
+function loadEmpresas() {
+    llamadaAjax("GET", "/api/empresas", null, function (err, data) {
+        if (err) return;
+        var empresas = [{ empresaId: 0, nombre: "" }].concat(data);
+        vm.posiblesEmpresas(empresas);
+    });
+}
+
+
+function loadDeparta() {
+    llamadaAjax("GET", "/api/departamentos/usuario/" + usuario.usuarioId, null, function (err, data) {
+        if (err) return;
+        var departamentos = [{ departamentoId: null, nombre: "" }].concat(data);
+        vm.posiblesDepartamentos(departamentos);
+    });
+}
+
+
+//modal asociación facturas
+
+function initTablaAsociarFacturas() {
+    tablaCarro = $('#dt_asociarFacturas').dataTable({
+        autoWidth: false,
+        paging: false,
+        "columnDefs": [ {
+            "targets": 0,
+            "orderable": false,
+            "width": "40%"
+            }],
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper_dt_basic) {
+                responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#dt_asociarFacturas'), breakpointDefinition);
+            }
+        },
+        rowCallback: function (nRow) {
+            responsiveHelper_dt_basic.createExpandIcon(nRow);
+        },
+        drawCallback: function (oSettings) {
+            responsiveHelper_dt_basic.respond();
+        },
+        language: {
+            processing: "Procesando...",
+            info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            infoFiltered: "(filtrado de un total de _MAX_ registros)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron resultados",
+            emptyTable: "Ningún dato disponible en esta tabla",
+            paginate: {
+                first: "Primero",
+                previous: "Anterior",
+                next: "Siguiente",
+                last: "Último"
+            },
+            aria: {
+                sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                sortDescending: ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        data: dataAsociarFacturas,
+        columns: [{
+            data: "facproveId",
+            width: "10%",
+            render: function (data, type, row) {
+                var html = '<label class="input">';
+                html += sprintf('<input id="chk%s" type="checkbox" name="chk%s" class="checkAll">', data, data);
+                //html += sprintf('<input class="asw-center" id="qty%s" name="qty%s" type="text"/>', data, data);
+                html += '</label>';
+                return html;
+            }
+        }, {
+            data: "emisorNombre"
+        }, {
+            data: "receptorNombre"
+        },  {
+            data: "numeroFacturaProveedor"
+        },{
+            data: "ref"
+        }, {
+            data: "fecha",
+            render: function (data, type, row) {
+                return moment(data).format('DD/MM/YYYY');
+            }
+        }, {
+            data: "total",
+            render: function (data, type, row) {
+                var string = numeral(data).format('0,0.00');
+                return string;
+            }
+        }, {
+            data: "totalConIva",
+            render: function (data, type, row) {
+                var string = numeral(data).format('0,0.00');
+                return string;
+            }
+        }, {
+            data: "formaPago"
+        }, {
+            data: "facproveId",
+            render: function (data, type, row) {
+                var bt2 = "<button class='btn btn-circle btn-success' onclick='editFactura(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var html = "<div class='pull-right'>" + bt2 + "</div>";
+                return html;
+            }
+        }]
+    });
+}
+
+function loadTablaAsociarFacturas(data) {
+    var dt = $('#dt_asociarFacturas').dataTable();
+    if (data !== null && data.length === 0) {
+        data = null;
+    }
+    dt.fnClearTable();
+    dt.fnAddData(data);
+    dt.fnDraw();
+    data.forEach(function (v) {
+        var field = "#chk" + v.facturaId;
+        if (v.sel == 1) {
+            $(field).attr('checked', true);
+        }
+        $(field).change(function () {
+            var quantity = 0;
+            var data = {
+                factura: {
+                    facproveId: v.facproveId,
+                    empresaId: v.empresaId,
+                    clienteId: v.clienteId,
+                    fecha: moment(v.fecha).format('YYYY-MM-DD'),
+                    sel: 0
+                }
+            };
+            if (this.checked) {
+                data.factura.sel = 1;
+            }
+            var url = "", type = "";
+            // updating record
+            var type = "PUT";
+            var url = sprintf('%s/api/facturasProveedores/%s', myconfig.apiUrl, v.facproveId);
+            $.ajax({
+                type: type,
+                url: url,
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+
+                },
+                error: function (err) {
+                    mensErrorAjax(err);
+                }
+            });
+        });
+    });
+}
+
+function aceptarBuscarAsociarFacturas() {
+    var mf = function () {
+        //if (!datosOK()) return;
+        vm.dFecha(moment().format('YYYY-MM-DD'));
+        vm.hFecha(moment().format('YYYY-MM-DD'));
+    
+        var proveedorId = 0
+        var departamentoId = 0;
+        if (vm.sdepartamentoId()) departamentoId = vm.sdepartamentoId();
+        var empresaId = 0;
+        if (vm.sempresaId()) empresaId = vm.sempresaId();
+      
+        var url = myconfig.apiUrl + "/api/facturasProveedores/correo/" + vm.dFecha() + "/" + vm.hFecha() 
+        + "/" + proveedorId 
+        + "/" + empresaId 
+        + "/"  + departamentoId 
+        + "/" + usuario.usuarioId;
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: "json",
+            contentType: "application/json",
+            success: function (data, status) {
+                loadTablaAsociarFacturas(data);
+                // mostramos el botén de alta
+                $("#btnAlta").show();
+                $('#checkMain').prop('checked', true);
+            },
+            error: function (err) {
+                mensErrorAjax(err);
+                // si hay algo más que hacer lo haremos aquí.
+            }
+        });
+    };
+    return mf;
+}
+
+
+
+function nuevaFacturaAsociada() {
+
+}
+
+
+function updateAll(opcion) {
+    var datos = null;
+    var sel = 0;
+    var tb = $('#dt_asociarFacturas').dataTable().api();
+    var datos = tb.rows( {page:'current'} ).data();
+    if(opcion) sel = 1
+    if(datos) {
+        for( var i = 0; i < datos.length; i++) {
+            var data = {
+                facprove: {
+                    facproveId: datos[i].facturaId,
+                    empresaId: datos[i].empresaId,
+                    clienteId: datos[i].clienteId,
+                    fecha: moment(datos[i].fecha).format('YYYY-MM-DD'),
+                    sel: sel
+            }
+        };
+                
+               
+        var url = "", type = "";
+         // updating record
+         var type = "PUT";
+         var url = sprintf('%s/api/facturasProveedores/%s', myconfig.apiUrl, datos[i].facproveId);
+            $.ajax({
+                type: type,
+                url: url,
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+
+                },
+                error: function (err) {
+                    mensErrorAjax(err);
+                }
+            });
+        }
+    }
 }
--- sql/2.3.122.sql
+++ sql/2.3.122.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `documentos_pago`(  
   `documentoPagoId` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(255),
-   `pdf` VARCHAR(255)
+   `pdf` VARCHAR(255),
   PRIMARY KEY (`documentoPagoId`)
 );
 
 ALTER TABLE `documentos_pago`   
   ADD COLUMN `numero` VARCHAR(255) NULL AFTER `documentoPagoId`;
 
-CREATE TABLE `proasistencia`.`documentosPago_facproves`(  
+CREATE TABLE `documentosPago_facproves`(  
   `docfacproveId` INT(11) NOT NULL AUTO_INCREMENT,
   `documentoPagoId` INT(11),
   `facproveId` INT(11),
