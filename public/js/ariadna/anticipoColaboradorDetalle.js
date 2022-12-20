22dd0b365e9d013de0dde3bad61da94f95ba7565
vicent <vicent@myariadna.com>
2022-12-20 10:22:07 +0100

    ajuste serviciadas correcto

--- public/js/ariadna/anticipoColaboradorDetalle.js
+++ public/js/ariadna/anticipoColaboradorDetalle.js
@@ -2173,9 +2173,11 @@ function nuevaServiciada() {
         acumulado = roundToTwo(acumulado);
         if(vm.antproveServiciadoId() != 0) {
             imp = acumulado - importeModificar + parseFloat(vm.importeServiciada());
+            imp.toFixed(2);
             
         } else {
             imp = acumulado + parseFloat(vm.importeServiciada());
+            imp.toFixed(2);
             if($('#chkCompleto').prop("checked")) {
                 tot = parseFloat(numeroDbf(vm.total()));
             } else {
@@ -2219,7 +2221,6 @@ function nuevaServiciada() {
         
     });
 }
-
 function datosOKServiciada() {
     if(vm.importeServiciada() == "") {
         vm.porcentaje(null);
