06cc044d8667d4aa7c06b797eb31d6851740455b
vicent <vicent@myariadna.com>
2023-02-17 09:56:50 +0100

    sql ajustadas

--- sql/2.3.141.sql
+++ sql/2.3.141.sql
@@ -1,35 +1,28 @@
-CREATE TABLE `ofertaDocumentacion`(  
-  `ofertaCarpetaId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `ofertaId` INT(11),
-  `carpetaId` INT(11),
-  `location` VARCHAR(255),
-  `key` VARCHAR(255),
-  PRIMARY KEY (`ofertaCarpetaId`),
-  CONSTRAINT `carpeta_ofertaFK` FOREIGN KEY (`ofertaId`) REFERENCES `ofertas`(`ofertaId`) ON UPDATE CASCADE
+CREATE TABLE `carpetas` (
+  `carpetaId` int(11) unsigned NOT NULL auto_increment,
+  `nombre` varchar(255) default NULL,
+  `tipo` varchar(255) default NULL,
+  PRIMARY KEY  (`carpetaId`)
 );
 
-CREATE TABLE `carpetas`(  
-  `carpetaId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(255),
-  PRIMARY KEY (`carpetaId`)
+CREATE TABLE `ofertaDocumentacion` (
+  `ofertaDocumentoId` int(11) unsigned NOT NULL auto_increment,
+  `ofertaId` int(11) default NULL,
+  `carpetaId` int(11) default NULL,
+  `location` varchar(255) default NULL,
+  `key` varchar(255) default NULL,
+  PRIMARY KEY  (`ofertaDocumentoId`),
+  KEY `carpeta_ofertaFK` (`ofertaId`),
+  CONSTRAINT `carpeta_ofertaFK` FOREIGN KEY (`ofertaId`) REFERENCES `ofertas` (`ofertaId`) ON UPDATE CASCADE
 );
 
-ALTER TABLE `carpetas`   
-  ADD COLUMN `url` VARCHAR(255) NULL AFTER `nombre`;
 
+INSERT INTO `carpetas` (`nombre`) VALUES ('Oferta_cliente'); 
+INSERT INTO `carpetas` (`nombre`) VALUES ('Costes_profesionales'); 
+INSERT INTO `carpetas` (`nombre`) VALUES ('Oferta_aceptada'); 
+INSERT INTO `carpetas` (`nombre`) VALUES ('Fotografias(Antes) '); 
+INSERT INTO `carpetas` (`nombre`) VALUES ('Fotografias(Despues) '); 
 
-INSERT INTO `carpetas` (`nombre`) VALUES ('Oferta cliente'); 
-INSERT INTO `carpetas` (`nombre`) VALUES ('Costes profesionales'); 
-INSERT INTO `carpetas` (`nombre`) VALUES ('Oferta aceptada'); 
-INSERT INTO `carpetas` (`nombre`) VALUES ('Fotografias (Antes) '); 
-INSERT INTO `carpetas` (`nombre`) VALUES ('Fotografias (Despues) '); 
-UPDATE `carpetas` SET `url` = 's3://comercializa-partes/Oferta cliente/' WHERE `carpetaId` = '1'; 
-UPDATE `carpetas` SET `url` = 's3://comercializa-partes/Costes profesionales/' WHERE `carpetaId` = '2'; 
-UPDATE `carpetas` SET `url` = 's3://comercializa-partes/Oferta aceptada/' WHERE `carpetaId` = '3'; 
-UPDATE `carpetas` SET `url` = 's3://comercializa-partes/Fotografias (Antes)/' WHERE `carpetaId` = '4'; 
-UPDATE `carpetas` SET `url` = 's3://comercializa-partes/Fotografias (Despues) /' WHERE `carpetaId` = '5'; 
-
- ALTER TABLE `proasistencia`.`carpetas` ADD COLUMN `tipo` VARCHAR(255) NULL AFTER `url`; 
 
  UPDATE carpetas SET tipo = 'oferta';
 
