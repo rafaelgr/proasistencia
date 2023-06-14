fc88901e0755aa6bc2cc1265dc095cf20e20cd0c
vicent <vicent@myariadna.com>
2023-06-14 10:40:27 +0200

    ajuste sql

--- sql/2.3.155.sql
+++ sql/2.3.155.sql
@@ -13,9 +13,9 @@ ALTER TABLE `tmp_contratos`
 UPDATE ofertas
 SET conceptosExcluidos = 
 'Quedan excluidos explícitamente de la presente oferta los siguientes trabajos:	
-	* Tasas e Impuestos ocasionados por los Proyectos e Instalaciones que se comprenden en la presente oferta
-	para sus respectivas legalizaciones ante los Organismos Oficiales.	
-	* El Impuesto sobre el Valor Añadido (IVA).	
-	* Apertura de calas, ensayos y/o estudio geotécnico.	
-	* Cualquier trabajo no especificado en la presente oferta.' 
+* Tasas e Impuestos ocasionados por los Proyectos e Instalaciones que se comprenden en la presente oferta
+para sus respectivas legalizaciones ante los Organismos Oficiales.	
+* El Impuesto sobre el Valor Añadido (IVA).	
+* Apertura de calas, ensayos y/o estudio geotécnico.	
+* Cualquier trabajo no especificado en la presente oferta.' 
 WHERE tipoOfertaId = 5 AND empresaId = 10
