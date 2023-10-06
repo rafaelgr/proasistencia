7923a9436394fc01043b535ead9b1445006231df
vicent <vicent@myariadna.com>
2023-10-06 11:18:12 +0200

    VERS 2.3.164 OK

--- sql/2.3.164.sql
+++ sql/2.3.164.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `facprove`   
   ADD COLUMN `externa` TINYINT(1) DEFAULT 0 NULL AFTER `esColaborador`;
+
+
+  INSERT INTO `tipos_operaciones` (`nombre`, `codopera`) VALUES ('REPERCUTIDA', '4')
