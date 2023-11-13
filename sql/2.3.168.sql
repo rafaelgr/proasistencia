6175e3f358d5e124adad4a0728c638387341fa93
vicent <vicent@myariadna.com>
2023-11-13 09:12:44 +0100

    sql ajustada

--- sql/2.3.168.sql
+++ sql/2.3.168.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `proasistencia`.`parametros`   
+ALTER TABLE `parametros`   
   ADD COLUMN `bucket_server` VARCHAR(255) NULL AFTER `raiz_url_docum`,
   ADD COLUMN `bucket_region_server` VARCHAR(255) NULL AFTER `bucket_server`,
   ADD COLUMN `bucket_folder_server` VARCHAR(255) NULL AFTER `bucket_region_server`,
