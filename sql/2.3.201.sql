68fa4f02239f8eb592002a061058de61f3b6c624
vicent <vicent@myariadna.com>
2024-12-12 09:34:25 +0100

    sql OK

--- sql/2.3.201.sql
+++ sql/2.3.201.sql
@@ -2,7 +2,7 @@ ALTER TABLE `contratos`
   ADD COLUMN `resumenExp` VARCHAR(255) NULL AFTER `fechaFinAlquiler`,
   ADD COLUMN `resumenJefeObraId` INT(11) NULL AFTER `resumenExp`,
   ADD COLUMN `resumenTecnicoId` INT(11) NULL AFTER `resumenJefeObraId`,
-  ADD COLUMN `resumenDistrito` VARCHAR(255) NULL AFTER `resumenTecnico`,
+  ADD COLUMN `resumenDistrito` VARCHAR(255) NULL AFTER `resumenTecnicoId`,
   ADD COLUMN `resumenPtoAceptado` TINYINT(1) DEFAULT 0 NULL AFTER `resumenDistrito`,
   ADD COLUMN `resumenAutorizacion` VARCHAR(255) NULL AFTER `resumenPtoAceptado`,
   ADD COLUMN `resumenActa` VARCHAR(255) NULL AFTER `resumenAutorizacion`,
@@ -27,4 +27,14 @@ CREATE TABLE `contrato_tecnicos`(
 ALTER TABLE `contrato_tecnicos`  
   ADD CONSTRAINT `contratoTecnicoFK` FOREIGN KEY (`tecnicoId`) REFERENCES `comerciales`(`comercialId`),
   ADD CONSTRAINT `tecnicoFK` FOREIGN KEY (`contratoId`) REFERENCES `contratos`(`contratoId`);
+
+
+CREATE TABLE `contratro_tasasVisado`(  
+  `tasaVisadoId` INT(11) NOT NULL AUTO_INCREMENT,
+  `contratoId` INT(11),
+  `titulo` VARCHAR(255) NOT NULL,
+  `contenido` TEXT NOT NULL,
+  PRIMARY KEY (`tasaVisadoId`),
+  CONSTRAINT `contratoTasasFK` FOREIGN KEY (`contratoId`) REFERENCES `contratos`(`contratoId`) ON UPDATE CASCADE ON DELETE CASCADE
+);
 
