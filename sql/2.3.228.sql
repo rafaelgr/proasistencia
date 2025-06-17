578fca07f16891ebb1e76e9fdf00285755c3dff7
vicent <vicent@myariadna.com>
2025-06-17 13:10:52 +0200

    VERS 2.3.228 OK

--- sql/2.3.228.sql
+++ sql/2.3.228.sql
@@ -3,3 +3,17 @@ ALTER TABLE `grupo_articulo`
 
 ALTER TABLE `articulos`   
 	ADD COLUMN `aplicarFormula` TINYINT(1) DEFAULT 1 NULL AFTER `esTecnico`;
+
+
+ALTER TABLE `propuestas`  
+  DROP FOREIGN KEY `propuesta_tipoProfresion`;
+
+
+ALTER TABLE `propuestas`   
+	CHANGE `tipoProfresionalId` `tipoProfesionalId` INT(11) NULL, 
+  DROP INDEX `propuesta_tipoProfresion`,
+  ADD  KEY `propuesta_tipoProfresion` (`tipoProfesionalId`);
+
+  ALTER TABLE `propuestas`  
+  ADD CONSTRAINT `propuesta_tipoProfesionFK` FOREIGN KEY (`tipoProfesionalId`) REFERENCES `tipos_profesionales`(`tipoProfesionalId`);
+
