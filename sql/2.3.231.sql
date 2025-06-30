609e9b06cf8f47b6e62c9a84c5ab129a22665902
vicent <vicent@myariadna.com>
2025-06-30 09:07:57 +0200

    SQL ajustado

--- sql/2.3.231.sql
+++ sql/2.3.231.sql
@@ -14,15 +14,10 @@ ALTER TABLE `profesiones_departamentos` ADD CONSTRAINT `profesionesDepartamentoP
 
 ALTER TABLE `profesiones_departamentos` DROP FOREIGN KEY `profesionesDepartamentosDepartamentoFK`;
 
-ALTER TABLE `profesiones_departamentos` ADD CONSTRAINT `profesionesDepartamentosDepartamentoFK` FOREIGN KEY (`departamentoId`) REFERENCES `departamentos`(`departamentoId`) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE `profesiones_departamentos` ADD CONSTRAINT `profesionesDepartamentosDepartamentoFK` FOREIGN KEY (`departamentoId`) 
+REFERENCES `departamentos`(`departamentoId`) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 ALTER TABLE `articulos`   
 	CHANGE `coste` `coste` DECIMAL(14,4) DEFAULT 0.00 NULL;
 
-
-
-ALTER TABLE `ofertas_lineas`   
-	CHANGE `totalLinea` `totalLinea` DECIMAL(14,4) NULL,
-	CHANGE `ventaNetaLinea` `ventaNetaLinea` DECIMAL(14,4) DEFAULT 0.00 NULL,
-	CHANGE `precio` `precio` DECIMAL(14,4) NULL;
