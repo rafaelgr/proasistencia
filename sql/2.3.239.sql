19340d77a92ae6d58cd40f93b8cd7ab221e5f33b
vicent <vicent@myariadna.com>
2025-09-11 14:14:14 +0200

    provisional

--- sql/2.3.239.sql
+++ sql/2.3.239.sql
@@ -1,2 +1,10 @@
 ALTER TABLE `empresas`   
 	ADD COLUMN `plantillaCorreoArqEncargo` TEXT NULL AFTER `plantillaCorreoArq`;
+
+	ALTER TABLE `propuestas`   
+	ADD COLUMN `fechaInicio` DATE NULL AFTER `fechaDocumentacion`;
+
+	ALTER TABLE `propuestas`   
+	ADD COLUMN `prevalorada` TINYINT(1) DEFAULT 0 NULL AFTER `fechaInicio`;
+
+
