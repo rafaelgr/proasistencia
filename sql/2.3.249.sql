93ccfb38304abf9593546f6d622345f378edd538
vicent <vicent@myariadna.com>
2025-11-07 17:27:08 +0100

    final dia

--- sql/2.3.249.sql
+++ sql/2.3.249.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `ofertas`   
 	ADD COLUMN `visulizaEnErp` TINYINT(1) DEFAULT 0 NULL AFTER `esAdicional`;
+
+ALTER TABLE `contratos`   
+	ADD COLUMN `visulizaEnErp` TINYINT(1) DEFAULT 0 NULL AFTER `resumenDiario`;
