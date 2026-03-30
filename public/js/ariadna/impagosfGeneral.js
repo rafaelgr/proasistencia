e2b0148447a7c33a065c1b52194de13525da2e16
vicent <vicent@myariadna.com>
2026-03-30 13:42:31 +0200

    provisdionbal

--- public/js/ariadna/impagosfGeneral.js
+++ public/js/ariadna/impagosfGeneral.js
@@ -38,18 +38,18 @@ function initForm() {
     ko.applyBindings(vm);
 
     // Recuperamos los departamentos de trabajo
-    recuperaDepartamento(function(err, data) {
-        if(err) return;
+    recuperaDepartamento(function (err, data) {
+        if (err) return;
         vm.posiblesDepartamentos(data);
         $("#cmbDepartamentosTrabajo").select2(select2Spanish());
     });
 
     $('#btnBuscarImpagos').click(buscarImpagos());
-    $('#btnGestionarImpagos').click(gestionarImpagos());
+    $('#btngenerarFacturasRectificativas').click(generarFacturasRectificativas());
     $('#frmBuscarImpagos').submit(function () { return false; });
 
-    $("#checkMain").click(function(e){
-        if($('#checkMain').prop('checked')) {
+    $("#checkMain").click(function (e) {
+        if ($('#checkMain').prop('checked')) {
             $('.checkAll').prop('checked', true);
             updateAll(true);
         } else {
@@ -120,24 +120,27 @@ function initTablaImpagos() {
         },
         data: dataImpagos,
         columns: [
-            { data: "facturaId", render: function (data) {
-                return '<label class="input"><input id="chk' + data + '" type="checkbox" class="checkAll"></label>';
-            }},
+            {
+                data: "facturaId", render: function (data) {
+                    return '<label class="input"><input id="chk' + data + '" type="checkbox" class="checkAll"></label>';
+                }
+            },
             { data: "emisorNombre" },
             { data: "receptorNombre" },
             { data: "dirTrabajo" },
             { data: "vNum" },
-            { data: "fecha", render: function(data) { return moment(data).format('DD/MM/YYYY'); }},
-            { data: "total", render: function(data) { return numeral(data).format('0,0.00'); }},
-            { data: "totalConIva", render: function(data) { return numeral(data).format('0,0.00'); }},
+            { data: "fecha", render: function (data) { return moment(data).format('DD/MM/YYYY'); } },
+            { data: "diasDesdeVencimiento" },
+            { data: "total", render: function (data) { return numeral(data).format('0,0.00'); } },
+            { data: "totalConIva", render: function (data) { return numeral(data).format('0,0.00'); } },
             { data: "formaPago" },
             { data: "observaciones" },
-            { data: "facturaId", render: function(data) {
-                var bt1 = "<button class='btn btn-circle btn-danger' onclick='deleteImpago(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
-                var bt2 = "<button class='btn btn-circle btn-success' onclick='editImpago(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
-                var bt3 = "<button class='btn btn-circle btn-success' onclick='printImpago(" + data + ");' title='Imprimir PDF'> <i class='fa fa-file-pdf-o fa-fw'></i> </button>";
-                return "<div class='pull-right'>" + bt1 + " " + bt2 + " " + bt3 + "</div>";
-            }}
+            {
+                data: "facturaId", render: function (data) {
+                    var bt1 = "<button class='btn btn-circle btn-success' onclick='editImpago(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                    return "<div class='pull-right'>" + bt1 + "</div>";
+                }
+            }
         ]
     });
 }
@@ -149,11 +152,18 @@ function loadTablaImpagos(data) {
     dt.fnAddData(data);
     dt.fnDraw();
 
-    data.forEach(function(v){
-        $("#chk" + v.facturaId).change(function() {
+    data.forEach(function (v) {
+        var field = "#chk" + v.facturaId;
+        if (v.sel == 1) {
+            $(field).attr('checked', true);
+        }
+       $(field).change(function () {
             var dataToUpdate = {
                 factura: {
                     facturaId: v.facturaId,
+                    empresaId: v.empresaId,
+                    clienteId: v.clienteId,
+                    fecha: moment(v.fecha).format('YYYY-MM-DD'),
                     sel: this.checked ? 1 : 0
                 }
             };
@@ -162,8 +172,8 @@ function loadTablaImpagos(data) {
                 url: myconfig.apiUrl + "/api/facturas/" + v.facturaId,
                 contentType: "application/json",
                 data: JSON.stringify(dataToUpdate),
-                success: function() {},
-                error: function(err){ mensErrorAjax(err); }
+                success: function () { },
+                error: function (err) { mensErrorAjax(err); }
             });
         });
     });
@@ -171,51 +181,51 @@ function loadTablaImpagos(data) {
 
 // Buscar impagos
 function buscarImpagos() {
-    return function() {
-        if(!datosOK()) return;
+    return function () {
+        if (!datosOK()) return;
 
         impagosCero = [];
         $.ajax({
             type: "GET",
-            url: myconfig.apiUrl + "/api/facturas/impagos/" 
-                 + spanishDbDate(vm.desdeFecha()) 
-                 + "/" + spanishDbDate(vm.hastaFecha()) 
-                 + "/" + vm.sdepartamentoId() // filtramos por departamento
-                 + "/" + usuario.usuarioId,
+            url: myconfig.apiUrl + "/api/facturas/impagos/"
+                + spanishDbDate(vm.desdeFecha())
+                + "/" + spanishDbDate(vm.hastaFecha())
+                + "/" + vm.sdepartamentoId() // filtramos por departamento
+                + "/" + usuario.usuarioId,
             dataType: "json",
             contentType: "application/json",
-            success: function(data){
-                if(data && data.length > 0) {
+            success: function (data) {
+                if (data && data.length > 0) {
                     loadTablaImpagos(data);
-                    $('#checkMain').prop('checked', false);
+                    $('#checkMain').prop('checked', true);
 
-                    data.forEach(function(f){
-                        if(f.total == 0) impagosCero.push(f.vNum);
+                    data.forEach(function (f) {
+                        if (f.total == 0) impagosCero.push(f.vNum);
                     });
-                    if(impagosCero.length>0) mensError("Las siguientes facturas tienen importe a cero:\n" + impagosCero.join("\n"));
+                    if (impagosCero.length > 0) mensError("Las siguientes facturas tienen importe a cero:\n" + impagosCero.join("\n"));
                 }
             },
-            error: function(err){ mensErrorAjax(err); }
+            error: function (err) { mensErrorAjax(err); }
         });
     };
 }
 
 // Gestionar impagos
-function gestionarImpagos() {
-    return function() {
-        if(!datosOK()) return;
+function generarFacturasRectificativas() {
+    return function () {
+        if (!datosOK()) return;
 
         $.ajax({
             type: "POST",
-            url: myconfig.apiUrl + "/api/facturas/gestionar/impagos/" 
-                 + spanishDbDate(vm.desdeFecha()) 
-                 + "/" + spanishDbDate(vm.hastaFecha()) 
-                 + "/" + vm.sdepartamentoId() // filtramos por departamento
-                 + "/" + usuario.usuarioId,
+            url: myconfig.apiUrl + "/api/facturas/generar/rectificativas/desde/facturas"
+                + spanishDbDate(vm.desdeFecha())
+                + "/" + spanishDbDate(vm.hastaFecha())
+                + "/" + vm.sdepartamentoId() // filtramos por departamento
+                + "/" + usuario.usuarioId,
             dataType: "json",
             contentType: "application/json",
-            success: function(data){
-                if(data!="OK") {
+            success: function (data) {
+                if (data != "OK") {
                     mensError("Error gestionando impagos: " + JSON.stringify(data));
                 } else {
                     mensNormal("Impagos gestionados correctamente");
@@ -224,7 +234,7 @@ function gestionarImpagos() {
                     loadTablaImpagos([]);
                 }
             },
-            error: function(err){ mensErrorAjax(err); }
+            error: function (err) { mensErrorAjax(err); }
         });
     };
 }
@@ -232,16 +242,24 @@ function gestionarImpagos() {
 // Checkbox todos
 function updateAll(opcion) {
     var sel = opcion ? 1 : 0;
-    var datos = $('#dt_impagos').dataTable().api().rows({page:'current'}).data();
-    for(var i=0; i<datos.length; i++){
-        var dataToUpdate = { factura: { facturaId: datos[i].facturaId, sel: sel }};
+    var datos = $('#dt_impagos').dataTable().api().rows({ page: 'current' }).data();
+    for (var i = 0; i < datos.length; i++) {
+        var dataToUpdate = {
+            factura: {
+                facturaId: datos[i].facturaId,
+                empresaId: v.empresaId,
+                clienteId: v.clienteId,
+                fecha: moment(v.fecha).format('YYYY-MM-DD'),
+                sel: sel
+            }
+        };
         $.ajax({
             type: "PUT",
             url: myconfig.apiUrl + "/api/facturas/" + datos[i].facturaId,
             contentType: "application/json",
             data: JSON.stringify(dataToUpdate),
-            success: function(){},
-            error: function(err){ mensErrorAjax(err); }
+            success: function () { },
+            error: function (err) { mensErrorAjax(err); }
         });
     }
 }
@@ -252,60 +270,3 @@ function editImpago(id) {
     window.open(url, '_new');
 }
 
-// Borrar impago
-function deleteImpago(id) {
-    $.SmartMessageBox({
-        title: "<i class='fa fa-info'></i> Mensaje",
-        content: "¿Desea eliminar este registro?",
-        buttons: '[Aceptar][Cancelar]'
-    }, function(btn){
-        if(btn=="Aceptar"){
-            $.ajax({
-                type: "DELETE",
-                url: myconfig.apiUrl + "/api/facturas/" + id,
-                contentType: "application/json",
-                success: function(){ buscarImpagos()(); },
-                error: function(err){ mensErrorAjax(err); }
-            });
-        }
-    });
-}
-
-// Imprimir impago
-function printImpago(id) {
-    $.ajax({
-        type: "GET",
-        url: myconfig.apiUrl + "/api/informes/facturas/" + id,
-        dataType: "json",
-        contentType: "application/json",
-        success: function(data){ informePDF(data); },
-        error: function(err){ mensErrorAjax(err); }
-    });
-}
-
-function informePDF(data){
-    var shortid = "HyGQ0yAP";
-    var postData = { template: { shortid: shortid }, data: data };
-    f_open_post("POST", myconfig.reportUrl + "/api/report", postData);
-}
-
-var f_open_post = function(verb, url, data, target){
-    var form = document.createElement("form");
-    form.action = url;
-    form.method = verb;
-    form.target = target || "_blank";
-
-    var input = document.createElement("textarea");
-    input.name = "template[shortid]";
-    input.value = data.template.shortid;
-    form.appendChild(input);
-
-    input = document.createElement("textarea");
-    input.name = "data";
-    input.value = JSON.stringify(data.data);
-    form.appendChild(input);
-
-    form.style.display = 'none';
-    document.body.appendChild(form);
-    form.submit();
-};
